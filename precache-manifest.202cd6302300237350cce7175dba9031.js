7a957a23967c79d752e5e4280ae6d0f627526c2d
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-10-14 16:55:25 -0400

    Updates

---
+++ precache-manifest.202cd6302300237350cce7175dba9031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7708d9ba5d00b56ed6e8faf1240ae65c",
+    "url": "/roasting_pans/index.html"
+  },
+  {
+    "revision": "2f0a37395886eb36a692",
+    "url": "/roasting_pans/static/css/main.6587be9e.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/roasting_pans/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "2f0a37395886eb36a692",
+    "url": "/roasting_pans/static/js/main.99b03fcd.chunk.js"
+  },
+  {
+    "revision": "682e50b1cab2fc78ccc1",
+    "url": "/roasting_pans/static/js/runtime~main.fa1d1e2e.js"
+  }
+]);
